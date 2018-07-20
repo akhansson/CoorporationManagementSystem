67eb8cdd1811340426284cdf322aa7bad630f45a
akhansson <ak.hansson88@gmail.com>
2018-07-20 16:56:20 +0200

    company class added

---
+++ CooperationApp/CooperationApp/Coorperation/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CooperationApp.Coorperation
+{
+    class Company
+    {
+        public string CompanyName { get; set; }
+    }
+}
