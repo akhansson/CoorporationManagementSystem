6afc52706a211a03f4882f6b488949f1389bdb12
akhansson <ak.hansson88@gmail.com>
2018-07-25 21:37:52 +0200

    fixat buggar

--- CooperationApp/CooperationApp/Models/Company.cs
+++ CooperationApp/CooperationApp/Models/Company.cs
@@ -8,9 +8,9 @@ namespace CooperationApp.Models
         public int Id { get; set; }
         public string CompanyName { get; set; }
 
-        //public override string ToString()
-        //{
-        //    return CompanyName;
-        //}
+        public override string ToString()
+        {
+            return CompanyName;
+        }
     }
 }
